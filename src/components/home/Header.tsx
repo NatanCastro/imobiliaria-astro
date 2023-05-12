7e67f15d47c6a7a4341f44c2e929c3072fb563ef
NatanCastro <castro.natan@outlook.com>
2023-05-12 17:28:26 -0300

    add target='_blank' to phone and whatsapp links

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -13,7 +13,7 @@ export const Header = () => {
       <h1 className='text-3xl'>LOGO</h1>
       <div className='flex items-center gap-14'>
         <ul className='flex items-center gap-4 text-2xl max-md:hidden'>
-          <a className='flex gap-4' href='https://wa.me/+0000000000000'>
+          <a className='flex gap-4' href='https://wa.me/+0000000000000' target='_blank'>
             <img
               className=''
               src='whatsapp.svg'
@@ -23,7 +23,7 @@ export const Header = () => {
             />
             (00) 00000-0000
           </a>
-          <a className='flex gap-4' href='tel:000000000000'>
+          <a className='flex gap-4' href='tel:000000000000' target='_blank'>
             <img className='' src='phone.svg' alt='telefone' width={24} height={24} />
             (00) 00000-0000
           </a>

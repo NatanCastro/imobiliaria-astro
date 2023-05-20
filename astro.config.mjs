4facdc593eb973e5ca74fc43b765ab401c774cd9
Natan Castro <castro.natan@outlook.com>
2023-05-20 13:32:56 -0300

    add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
-import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/edge";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel/edge'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
 
-import react from "@astrojs/react";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
-  integrations: [tailwind(), react()],
-});
+  output: 'server',
+  adapter: vercel({
+    analytics: true
+  }),
+  integrations: [tailwind(), react()]
+})

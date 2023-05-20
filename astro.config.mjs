f2f090799e4143ab21888f7af7184e5bfa47263d
Natan Castro <castro.natan@outlook.com>
2023-05-20 13:45:28 -0300

    reconfigure vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import vercel from '@astrojs/vercel/edge'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 
-import vercel from '@astrojs/vercel/serverless'
-
 // https://astro.build/config
 export default defineConfig({
   output: 'server',

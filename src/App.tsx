2f7025b0322335210deb945bbbcb35441a2b253d
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2023-06-13 20:05:21 -0300

    add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router-dom'
 import 'primereact/resources/primereact.min.css'
 import Router from './routes'
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
   return (
     <BrowserRouter>
       <Router />
+      <Analytics />
     </BrowserRouter>
   )
 }

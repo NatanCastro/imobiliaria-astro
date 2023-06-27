26985a6b379b811c67dc8c2f80a4aec0ecb532e8
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2023-06-27 12:21:09 -0300

    remove web vitals

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,9 @@ import 'primereact/resources/primereact.min.css'
 import 'primereact/resources/themes/tailwind-light/theme.css'
 import 'primeicons/primeicons.css'
 import './index.css'
-import reportWebVitals from './reportWebVitals.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
-
-reportWebVitals()
